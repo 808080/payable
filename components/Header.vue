bd8e54d204427347ac935be8298548c824f2447a
szaderaka <serzaderaka@gmail.com>
2024-04-29 21:55:28 +0300

    header responsive

--- components/Header.vue
+++ components/Header.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import IconMenu from '~/assets/images/menu.svg';
+import IconClose from '~/assets/images/close.svg';
+
 const links = [
   {
     label: 'Products',
@@ -17,28 +20,43 @@ const links = [
     to: '/about'
   }
 ];
+
+const isOpen = ref(false);
+const nav = ref<HTMLDivElement>();
 </script>
 
 <template>
-  <header class="bg-gradient-to-b from-secondary via-secondary to-secondary/0 pt-3 sticky top-0">
-    <div class="max-w-[1706px] p-3 mx-auto flex items-center justify-between rounded-2xl bg-secondary/30 backdrop-blur-md">
-      <NuxtLink to="/">
-        <Logo />
-      </NuxtLink>
+  <header class="bg-gradient-to-b from-secondary via-secondary to-secondary md:to-secondary/0 lg:pt-3 sticky top-0">
+    <div
+      class="max-w-[1706px] p-3 mx-auto lg:flex items-center justify-between md:rounded-2xl bg-secondary/30 md:backdrop-blur-md relative">
+      <div class="flex justify-between items-center p-2 lg:p-0">
+        <NuxtLink to="/">
+          <Logo />
+        </NuxtLink>
 
-      <nav class="max-w-[600px] w-full">
-        <ul class="flex items-center text-center">
-          <li v-for="{ label, to } in links" :key="to" class="w-full">
-            <NuxtLink :to class="p-2 hover:text-primary duration-200 font-bold">
-              {{ label }}
-            </NuxtLink>
-          </li>
-        </ul>
-      </nav>
+        <button type="button" @click="isOpen = !isOpen" class="lg:hidden w-10 h-10">
+          <img :src="isOpen ? IconClose : IconMenu" alt="" class="block w-full">
+        </button>
+      </div>
 
-      <div class="flex">
-        <Button label="Log in" to="#" variant="transparent" class="mr-2.5" />
-        <Button label="Contact Us" to="#" />
+      <div class="absolute left-0 bg-secondary w-full lg:static overflow-hidden max-h-0 lg:!max-h-full duration-300"
+        :style="`max-height: ${isOpen ? nav?.offsetHeight : 0}px`">
+        <div ref="nav" class="p-3 lg:p-0 lg:flex justify-between w-full ">
+          <nav class="lg:max-w-[700px] w-full mx-auto max-h-[400px] overflow-y-auto">
+            <ul class="lg:flex items-center lg:text-center text-2xl lg:text-base">
+              <li v-for="{ label, to } in links" :key="to" class="w-full">
+                <NuxtLink :to class="p-2 hover:text-primary duration-200 font-bold block">
+                  {{ label }}
+                </NuxtLink>
+              </li>
+            </ul>
+          </nav>
+
+          <div class="justify-end sm:flex mt-3 lg:mt-0">
+            <Button label="Log in" to="#" variant="transparent" class="sm:mr-2.5 w-full sm:w-auto" />
+            <Button label="Contact Us" to="#" class="mt-2 sm:mt-0 w-full sm:w-auto" />
+          </div>
+        </div>
       </div>
     </div>
   </header>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="">
     home
 
-    <h1 class="text-9xl">
+    <h1 class="text-5xl">
       Welcome to payabl.
 an omniverse of payments
     </h1>
